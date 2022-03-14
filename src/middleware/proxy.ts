fd92bce5e25e46f53ccd822aa6c02a2c8ec9622a
Dave Smith <dave@Daves-MBP-2.fios-router.home>
2022-03-14 15:35:47 -0400

    Remove proxy from package.json

--- src/middleware/proxy.ts
+++ src/middleware/proxy.ts
@@ -1,13 +1,12 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
-import cors from 'cors'
 
 // Handles CORs errors when requesting data from Alpha Vantage
-module.exports = (app) => {
-  const corsOptions = {
-    origin: 'https://stock-comparison-tool.herokuapp.com/',
-    optionsSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
-  app.options('*', cors())
-  app.use('/')
+module.exports = app => {
+  app.use(
+    createProxyMiddleware('/api/*',
+      {
+        target: 'https://stock-comparison-tool.herokuapp.com',
+        changeOrigin: true
+      })
+  )
 }
