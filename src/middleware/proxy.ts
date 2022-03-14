7b5210de5fa47aa22cfb86012be822a32c8b22a5
Dave Smith <dave@Daves-MBP-2.fios-router.home>
2022-03-14 14:54:50 -0400

    Update proxy

--- src/middleware/proxy.ts
+++ src/middleware/proxy.ts
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 // Handles CORs errors when requesting data from Alpha Vantage
 module.exports = app => {
   app.use(
-    createProxyMiddleware('api',
+    createProxyMiddleware('*',
       {
         target: 'https://alphavantage.co',
         changeOrigin: true
